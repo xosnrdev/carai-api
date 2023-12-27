7de5f3552d81c968addb3057155f2258c06fab37
xosnrdev <successxodev@gmail.com>
2023-12-27 07:09:06 -0500

    ci: ci/cd pipeline configuration with docker for aws ecs farget deployment

--- src/middlewares/rate-limit-middleware.ts
+++ src/middlewares/rate-limit-middleware.ts
@@ -7,7 +7,7 @@ import "dotenv/config";
  */
 function rateLimitMiddleware() {
   return rateLimit({
-    windowMs: Number(process.env.RATE_LIMIT_WINDOW_MS),
+    windowMs: Number(process.env.RATE_LIMIT_WINDOW_MS) || 900000,
     max: Number(process.env.RATE_LIMIT_MAX),
   });
 }
