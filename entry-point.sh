73d021d45300c222618cda668db12be75d8cd16a
xosnrdev <successxodev@gmail.com>
2023-12-29 07:25:11 -0500

    feat(ci/cd): Integrate Docker Compose with ECS deployment and ECR image management
    This commit enhances the CI/CD pipeline to build Docker images using Docker Compose, tag and push them to Amazon ECR, and deploy the updated services to Amazon ECS. The Docker Compose file has been updated for ECS compatibility, including environment optimizations and security considerations. Additionally, the CI/CD workflow now dynamically tags images based on the changelog version, ensuring a consistent and traceable deployment process.
    
    - Update GitHub Actions workflow to use Docker Compose for image building, tagging, and pushing to ECR.
    - Modify Docker Compose file to remove the Docker socket volume when unnecessary for improved security.
    - Implement dynamic image tagging in the CI/CD pipeline based on changelog versioning.
    - Configure Docker context for ECS in the GitHub Actions workflow for seamless deployment.
    - Ensure all modifications adhere to industry standards for Docker and ECS integration.
    
    BREAKING CHANGE: The deployment process now fully relies on Docker Compose and ECS context. Ensure existing deployment configurations are compatible with these changes.

---
+++ entry-point.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Constants
+S3_ENV_FILE="s3://carai-api-657214731516/variables.env"
+LOCAL_ENV_FILE="/app/.env"
+MAX_RETRIES=5
+
+# Function to log messages with a timestamp
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# Function to fetch environment variables file from S3 with retries
+fetch_env_from_s3() {
+  attempt=0
+  until [ "$attempt" -ge "$MAX_RETRIES" ]; do
+    log "Attempting to fetch environment variables from S3 ($attempt/$MAX_RETRIES)"
+    if aws s3 cp "$S3_ENV_FILE" "$LOCAL_ENV_FILE"; then
+      log "Successfully fetched the environment variables."
+      return 0
+    fi
+    attempt=$((attempt + 1))
+    sleep 10
+  done
+
+  log "Failed to fetch environment variables after $MAX_RETRIES attempts."
+  return 1
+}
+
+# Main execution starts
+log "Starting entry point script."
+
+# Fetch environment variables
+if ! fetch_env_from_s3; then
+  log "Exiting due to failure in fetching environment variables."
+  exit 1
+fi
+
+# Source the variables to the current environment
+if [ -f "$LOCAL_ENV_FILE" ]; then
+  log "Sourcing environment variables."
+  . "$LOCAL_ENV_FILE"
+else
+  log "Environment file not found."
+  exit 1
+fi
+
+# Execute the main process of the container
+log "Executing the main process."
+exec "$@"
